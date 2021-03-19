36219225339cc7d5c38e70c683b3f7e5a96a1726
asanch31 <78468358+asanch31@users.noreply.github.com>
2021-03-18 20:41:38 -0700

    Unit 3 Lesson A

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obsPrefab;
+    private Vector3 spawnPos = new Vector3(20, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObj",2,2);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnObj()
+    {
+        Instantiate(obsPrefab, spawnPos, obsPrefab.transform.rotation);
+    }
+}
